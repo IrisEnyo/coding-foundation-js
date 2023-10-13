777ae147a8b5ae0979fa4a2d17ae5d48dff90b3a
Irina H <ischa15@web.de>
2023-10-13 12:26:01 +0200

    chore: trys different console.log methods

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+console.log("Hello World!".length);
+console.log("hello world!".toUpperCase());
+console.log(1 + 1);
+console.log(37);
+console.log((36.7).toFixed());
+console.log((36.729).toFixed(2));
