5b319669b1bce36efe58c164a343a7263f654760
Irina H <ischa15@web.de>
2023-10-17 07:48:14 +0200

    chore: solved task

--- operators-data-types/script.js
+++ operators-data-types/script.js
@@ -1,1 +1,34 @@
 console.log("Irina");
+
+// 01)
+console.log("I" + "r" + "i" + "n" + "a");
+
+// 02)
+/* Error! Make it work
+console.log("Hallo"; */
+console.log("Hallo");
+
+/* Error! Make it work
+console.log("That doesn"t work"); */
+console.log("That doesn´t work");
+
+/* should return true. Change only the operator to fix it.
+console.log(50 + 2 > 52); */
+console.log(50 + 2 == 52);
+
+/* should be false. Change only the operator to fix it.
+console.log("333" == 333); */
+console.log("333" > 333);
+
+/* Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log(600 + 510 / 2); */
+console.log((600 + 510) / 2);
+
+/* Wrong result: Expected a Number 4 received 22
+console.log("2" + 2); */
+console.log(2 + 2);
+
+// 03)
+/* console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5 */
+console.log(25 / 5 === 50 / 10);
